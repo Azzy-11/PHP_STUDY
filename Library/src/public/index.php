d6f7a2b7670a02978e132e5b82ae85cd44219a11
Azzy-11 <azu11.mermona@gmail.com>
2024-07-28 23:00:30 +0900

    🚧 ログインページ雛型のみ作成

---
+++ Library/src/public/index.php
@@ -0,0 +1,20 @@
+<?php
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Library Login</title>
+</head>
+<body>
+  <form action="" method="post">
+    <label for="loginId">メールアドレス</label>
+    <input type="email" name="loginId">
+    <label for="loginPw">パスワード/label>
+    <input type="password" name="loginPw">
+    <button type="submit">ログイン</button>
+  </form>
+  <a href="">新規登録はこちらから</a>
+</body>
+</html>
