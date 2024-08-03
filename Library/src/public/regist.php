60e31b6146c17180f5c73cc37c58d1e4b8e86ba8
Azzy-11 <azu11.mermona@gmail.com>
2024-08-03 22:57:29 +0900

    🩹 HTML微修正

--- Library/src/public/regist.php
+++ Library/src/public/regist.php
@@ -15,17 +15,14 @@ $_SESSION['csrf'] = bin2hex(random_bytes(16));
 <body>
   <h1>新規登録</h1>
   <form action="comfirm.php" method="post">
-    <label for="name">氏名</label><br>
-      <label for="surName">　姓</label>
-      <input type="text" name="surName" id="surName">
-      <label for="firstName">名</label>
-      <input type="text" name="firstName" id="firstName"><br>
+    <label for="name">名前</label><br>
+    <input type="text" name="name" id="name"><br>
     <label for="email">メールアドレス</label><br>
     <input type="email" name="email" id="email"><br>
     <label for="password">パスワード</label><br>
     <input type="password" name="password" id="password"><br>
-    <label for="confirm_password">パスワードの確認</label><br>
-    <input type="password" name="confirm_password" id="confirm_password"><br>
+    <label for="re:password">パスワードの確認</label><br>
+    <input type="password" name="re:password" id="re:password"><br>
     <input type="hidden" name="csrfToken" value="<?php echo $_SESSION['csrf']; ?>"><br>
     <button type="submit">確認</button>
   </form>
