6db3cf6a2807610495d753d7b88c0eb04d08042d
Azzy-11 <azu11.mermona@gmail.com>
2024-08-17 23:02:29 +0900

    ✨ ログインリクエストにcsrf記載

--- Library/src/public/login.php
+++ Library/src/public/login.php
@@ -1,4 +1,10 @@
 <?php
+declare(strict_types=1);
+session_start();
+
+require_once('../libs/Csrf.php');
+
+Csrf::setToken();
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -8,11 +14,13 @@
   <title>Library Login</title>
 </head>
 <body>
-  <form action="" method="post">
+  <form action="index.php" method="post">
     <label for="loginId">メールアドレス</label>
     <input type="email" name="loginId">
     <label for="loginPw">パスワード/label>
     <input type="password" name="loginPw">
+    <input type="hidden" name="csrfToken" value="<?php echo $_SESSION['csrf']; ?>">
+    <input type="hidden" name="type" value="201">
     <button type="submit">ログイン</button>
   </form>
   <a href="regist.php">新規登録はこちらから</a>
