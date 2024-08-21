311416e6d6456cfa5984a5513ec8047ed5d56c5c
Azzy-11 <azu11.mermona@gmail.com>
2024-08-21 23:20:40 +0900

    ✨ 書籍登録ページ作成

--- Library/src/public/top.php
+++ Library/src/public/top.php
@@ -8,6 +8,7 @@ require_once('../libs/Auth.php');
 Auth::checkAuth();
 Csrf::setToken();
 
+$priv = (isset($_SESSION['user']['admin']) && is_int($_SESSION['user']['admin'])) ? $_SESSION['user']['admin'] : 0;
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -22,5 +23,17 @@ Csrf::setToken();
     <input type="hidden" name="type" value="202">
   </form>
   <a href="#" onclick="document.getElementById('logout').submit(); return false;">ログアウト</a>
+
+  <li>
+    <?php
+    if ($priv === 1) {
+      echo '<li><a href="admin/regBook.php">書籍登録</a></li>';
+    }
+    ?>
+    <li><a href="">書籍レンタル（一覧表設置）</a></li>
+    <li><a href="">書籍返却</a></li>
+    <li><a href="">マイページ</a></li>
+  </li>
+
 </body>
 </html>
